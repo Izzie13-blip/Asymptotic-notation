8d9b754bd710724eaad9c818a9c6a96cd3db856e
Izzie13-blip <marketbyamie@gmail.com>
2025-06-19 19:41:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration=1
+    print("The total number of iteration is ",n)
+    
+printnumber(10)
+printnumber(20)
