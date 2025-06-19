3d619b7fa1df4c89fa05ad03805297ab94ca58cf
Izzie13-blip <marketbyamie@gmail.com>
2025-06-19 19:49:42 +0100

    Add files via upload

---
+++ min.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    for i in range(1,n+1):
+        print("The number entered by user is ",n)
+        iteration=1
+    print("The total number of iteration is ",n)
+    
+printnumber(10)
+printnumber(20)
